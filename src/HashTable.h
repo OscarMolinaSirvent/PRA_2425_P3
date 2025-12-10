87699cd15aaeb8d041a6f60b0377ed6186832a88
OscarMolinaSirvent <omolsir@upv.edu.es>
2025-12-10 17:52:10 +0100

    Add files via upload

--- src/HashTable.h
+++ src/HashTable.h
@@ -53,11 +53,13 @@ class HashTable: public Dict<V> {
         void insert(std::string key, V value) override{
             int index = h(key);
             TableEntry<V> insert(key,value);
-            try{
-                search(key);
-            }catch(std::runtime_error &e){
+            int position = table[index] -> search(insert);
+            
+            if(position == -1){
                 table[index] -> append(insert);
                 n++;
+            }else{
+                throw std::runtime_error("La KEY ya existe.");
             }
         }
 
@@ -66,7 +68,7 @@ class HashTable: public Dict<V> {
             TableEntry<V> buscar(key);
             int position = table[index] -> search(buscar);
             if(position == -1){
-                throw std::runtime_error("No se pudo encontrar el key.");
+                throw std::runtime_error("No se pudo encontrar el KEY.");
             }else{
                 return table[index] -> get(position).value;
             }
@@ -77,11 +79,12 @@ class HashTable: public Dict<V> {
             TableEntry<V> eliminar(key);
             int position = table[index] -> search(eliminar);
             if(position == -1){
-                throw std::runtime_error("No se ha podido encontrar el par clave -> valor");
+                throw std::runtime_error("No se ha podido encontrar el par KEY -> VALUE");
             }else{
                 V v;
                 v = table[index] -> get(position).value;
                 table[index] -> remove(position);
+                n--;
                 return v;
             }
         }
@@ -106,7 +109,7 @@ class HashTable: public Dict<V> {
         V operator[](std::string key){
             int index = h(key);
             if(index >= capacity()){
-                throw std::runtime_error("La posicion no existe");
+                throw std::runtime_error("La POSICIÓN no existe");
             }else{
                 return search(key);
             }
